b1264630dda44101cc05ad669ec3221e6e95ad24
Haywhyoh <adefeyisayo998@gmail.com>
2022-07-19 08:20:17 -0700

    fixed betty

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,75 +1,84 @@
 #include "sort.h"
+
 /**
- * counting_sort - sorting array of ints
- * @array: array of ints
- * @size: size of array
- *
+ * _bigest -  Give me the largest number in a array of integers
+ * @a: The Int array
+ * @size: Size of array
+ * Return: The largest number
+ */
+int _bigest(int *a, size_t size)
+{
+	size_t i;
+	int k = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (k < a[i])
+			k = a[i];
+	}
+	return (k);
+}
+/**
+ * _memset -  Create a integer array and set each space in 0
+ * @size: Size of array
+ * Return: The integer array
+ */
+int *_memset(int size)
+{
+	int *ptr = NULL;
+	int i;
+
+	ptr = malloc((size) * sizeof(int));
+
+	for (i = 0; i < size; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
+/**
+ * counting_sort - sort an array with the counting sort algorithm
+ * @array: The Int array
+ * @size: Size of array
+ * Return: The sorted list
  */
 void counting_sort(int *array, size_t size)
 {
-	int *copy, *counting;
-	unsigned int max, i, tmp;
+	size_t i, j;
+	int k = 0;
+	int *aux = NULL, *array_sort = NULL;
 
-	if (size == 0 || size == 1)
+	if (size < 2)
 		return;
-
-	i = 0;
-	max = 0;
-	while (i < size)
-	{
-		if (array[i] > (int)max)
-			max = array[i];
-		i++;
-	}
-	max++;
-	counting = malloc(max * sizeof(int));
-	if (counting == NULL)
+	/*Conocemos el numero mas grande del array*/
+	k = _bigest(array, size);
+	/*ayuda a crear el array*/
+	aux = _memset(k + 1);
+	if (!aux)
 		return;
-	copy = malloc(size * sizeof(int));
-	if (copy == NULL)
+	/*Establecer los valores para ordenar*/
+	for (i = 0; i < size; i++)
+		for (j = 0; (int)j < k + 1; j++)
+			if ((int)j == array[i])
+				aux[j] += 1;
+	/*Modifica el contador en el arrary*/
+	for (i = 0; (int)i < k; i++)
+		aux[i + 1] = aux[i] + aux[i + 1];
+	print_array(aux, k + 1);
+	/*Crea el array ordenada*/
+	array_sort = malloc(size * sizeof(int));
+	if (!array_sort)
 	{
-		free(counting);
+		free(aux);
 		return;
 	}
-
-	i = 0;
-	while (i < size)
+	for (i = 0; i < size; i++)
 	{
-		copy[i] = array[i];
-		i++;
+		array_sort[aux[array[i]] - 1] = array[i];
+		aux[array[i]] -= 1;
 	}
+	for (j = 0; j < size; j++)
+		array[j] = array_sort[j];
 
-	i = 0;
-	while (i < max)
-	{
-		counting[i] = 0;
-		i++;
-	}
-
-	i = 0;
-	while (i < size)
-	{
-		counting[array[i]]++;
-		i++;
-	}
-
-	i = 1;
-	while (i < max)
-	{
-		counting[i] += counting[i - 1];
-		i++;
-	}
-
-	print_array(counting, max);
-
-	i = 0;
-	while (i < size)
-	{
-		tmp = copy[i];
-		array[counting[tmp] - 1] = tmp;
-		counting[tmp]--;
-		i++;
-	}
-	free(counting);
-	free(copy);
+	free(array_sort);
+	free(aux);
 }
